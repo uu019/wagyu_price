aaba2a7807fddda2d58869589f74bd9f77d88133
ushiyama <ushiyama.h12@gmail.com>
2022-12-14 18:55:27 +0900

    スマホサイズで表示したときのスクロールボタンを修正

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
-$(function(){
-  var w = $(window).width();
-  var x = 576;
-  if(w < x)
-  {
-  $(function() {
-    $('#page-top a').click(function(e) {
-      e.preventDefault();
-      var position = 0;
-      var speed = 500;
-      $('html, body').animate({
-        scrollTop: position,
-      }, speed, 'swing');
-      return false;
-    });
+$(function() {
+  $('#page-top a').click(function(e) {
+    e.preventDefault();
+    var position = 0;
+    var speed = 500;
+    $('html, body').animate({
+      scrollTop: position,
+    }, speed, 'swing');
+    return false;
   });
-  }
-  else
-  {
-  $('#page-top').hide();
-  }
 });
